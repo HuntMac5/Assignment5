4d9e8bec6482d1bf24ff1ec0f3b3c784cf2f6215
HuntMac5 <huntermccall.hm@gmail.com>
2025-05-01 18:12:46 -0500

    all methods are finished, time to debug

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,6 @@ public class Main {
 
         // Solve the puzzle
         sudoku.solve();
+        sudoku.printBoard();
     }
 }
--- src/sudoku/Sudoku.java
+++ src/sudoku/Sudoku.java
@@ -42,11 +42,42 @@ public class Sudoku {
         }
         return false;}
 
-    public void solve(){}
+    public void solve(){
+        System.out.println("\nSolving sudoku...");
+
+        if(solveSudoku(0, 0)) {
+            System.out.println("Solved!");
+            printBoard();
+        }else{
+            System.out.println("\nPuzzle could not be solved.\nNo answer exists.");
+        }
+    }
     public boolean isSafe(int row, int col, int numb){
+        for(int i=0; i<SIZE; i++){
+            if(board[row][i] == numb || board[i][col] == numb){
+                return false;
+            }
+        }
+
+        int sRow = row - row%3;
+        int sCol = col - col%3;
+        for (int i=0; i<3; i++){
+            for (int j=0; j<3; j++){
+                if(board[sRow+i][sCol+j] == numb){
+                    return false;
+                }
+            }
+        }
         return true;
     }//selected num in cell
-    public void printBoard(){}
+    public void printBoard(){
+        for (int i=0; i<SIZE; i++){
+            for (int j=0; j<SIZE; j++){
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
     public int[][] getBoard(){return this.board;}
     public void setBoard(int[][] board){this.board = board;}
